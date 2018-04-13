35fcd7224d5a2ede686c35461129be1cca64e524
Marcus Münger <marcus.munger@cybercom.com>
2018-04-13 23:44:24 +0200

    switched to microbundle

---
+++ src/main/java/se/bnpo/apiindex/rest/RestApplication.java
@@ -0,0 +1,11 @@
+package se.bnpo.apiindex.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author Marcus Münger
+ */
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+}
